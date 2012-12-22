d21961b1736348dea9d1db3133714e3315332921
azeem <azeem@sunglass.io>
2012-12-22 17:27:51 +0530

    Post table

---
+++ src/main/scala/code/model/Post.scala
@@ -0,0 +1,12 @@
+package code.model
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: z33m
+ * Date: 12/22/12
+ * Time: 5:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Post {
+
+}
