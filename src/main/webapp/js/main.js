0e83efcf60a01c437900c6118aeb4c89dd78d534
Sushant Bhatia <bhatia.sushant24@gmail.com>
2012-12-22 17:48:06 +0530

    WIP autosuggest

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,1 +1,34 @@
+$(function(){
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $("#tags").autocomplete({
+        source:availableTags
+    });
+    $("#tags").on( "autocompleteselect", function( event, ui ) {
+        console.log($(this).val());
+    } );
 
+
+});
