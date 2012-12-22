dcb8532ce29aba1837b8c96821ed1edbc70a0fa0
dpatro <patro@sunglass.io>
2012-12-22 19:54:37 +0530

    Post Creation Snippet

---
+++ src/main/scala/code/snippet/CreatePost.scala
@@ -0,0 +1,16 @@
+package code.snippet
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: comet
+ * Date: 22/12/12
+ * Time: 7:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CreatePost {
+  // Post elements here
+
+  def process() {
+
+  }
+}
