6d94226abe594b443bfca9789ff0d70b4c55a71f
azeem <azeem@sunglass.io>
2012-12-22 18:21:32 +0530

    Login basics

---
+++ src/main/scala/code/lib/SessionData.scala
@@ -0,0 +1,18 @@
+package code.lib
+
+import net.liftweb.http.SessionVar
+import net.liftweb.common._
+import code.model.SJUser
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: z33m
+ * Date: 12/22/12
+ * Time: 6:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object SessionData {
+  object currentUser extends SessionVar[Box[SJUser]](Empty)
+}
