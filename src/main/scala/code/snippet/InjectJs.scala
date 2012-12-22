b52fe759a36adeb5e68ccd994def62539731134f
Aditya Vishwakarma <blackleopard92@gmail.com>
2012-12-22 21:22:43 +0530

    fuck

---
+++ src/main/scala/code/snippet/InjectJs.scala
@@ -0,0 +1,24 @@
+package code.snippet
+
+import net.liftweb._
+import http.js.JE.Str
+import http.js.JsCmds.{Run, Script}
+import json._
+import code.model.Tag
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dasher
+ * Date: 12/22/12
+ * Time: 9:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class InjectJs {
+
+  def render = {
+    println("Injecting shit into page")
+    val tags = Tag.findAll().map(_.text.is).mkString("[\"", "\",\"", "\"]")
+    val res = Script(Run("var availableTags = " + tags + ";"))
+    <head>{res}</head>
+  }
+}
